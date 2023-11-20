04d5491d94f7a7b2d7ac1cc319ad020171020928
Bishop <christianiradukunda6@gmail.com>
2023-11-20 22:04:57 +0200

    worked with types and interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
-//array with numbers
-const a:number[] =[1,2,3]
-a.push(4)
 
-// array with strings
-const b:string[] = ['Chris','Iradukunda']
-b.push("Hano Media")
+// working with types
+type Person = {
+    name:string;
+    age:number;
+    isProgrammer?:boolean;
+    friends : string[]
+
+} 
+const person:Person = {
+    name:"Chris",
+    age:30,
+    friends:[]
+}
+person.isProgrammer = true
+//accessing values in the object
+person.name;
+person.age
+person.isProgrammer
+const person2:Person = {
+    name:"Christian",
+    age:30,
+    friends:[]
+}
+/* we used the optional chaining "?" to indicate that the value for isProgrammer is 
+ optional*/
+
+ console.log(person.name)
+ console.log(person.age)
+ console.log(person.isProgrammer)
