2f9bb2196f5b83c728428308bf7a6ae1fe6d0c9c
Bishop <christianiradukunda6@gmail.com>
2023-11-21 21:56:29 +0200

    worked with unions

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-// working with functions
-function sum(a:number,b:number){
-    return a + b
-}
-//calling the function
-const c = sum(2,3)
+// working with unions
+// a variable can be either a string or a number or any other defined type
+let id : string | number
+// we can re-assign the id as a number or a string
+id = "1"
+id=7
 
-//function inputing the object
-function printPerson(person:{name:string}){
-    console.log(person.name);
+// example2: using unions on default values
+type Todo ={
+    name:string,
+    status:"complete" | "in progress" | "draft"
 }
-// calling the function
-printPerson({name:"Christian"})
 
+// if we try to initialize a variable we have to only work with pre-defined statuses
+
+const todo:Todo = {name:"Fix bugs",status:"complete"}
