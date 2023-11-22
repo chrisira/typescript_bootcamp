e99626d1e30316c65f876834d26ae73651df9977
Bishop <christianiradukunda6@gmail.com>
2023-11-22 23:47:48 +0200

    working with index(start)

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,11 @@
-// working with Intersection
-// a type can have a combined data types
 type Person ={
     name:string;
-    age:number;
+    skillLevel:"Beginner" | "Intermediate" | "Expert"
 }
-type PersonWithId = Person & {id:string}
 
-const person :PersonWithId = {id:"fjsdjflkdjkf",name:"Christian",age:30}
+const person:Person = {name:"Chris",skillLevel:"Expert"}
+printSkillLevel(person.skillLevel);
 
-// using interface
-
-interface Person2{
-    name:string;
-    age:number;
+function printSkillLevel(skillLevel:"Beginner"|"Intermediate"|"Expert"){
+    console.log(skillLevel);
 }
-interface PersonWithId2 extends Person2 {id:string}
-
-const person2 :PersonWithId = {id:"fjsdjflkdjkf",name:"Christian",age:30}
-
-
