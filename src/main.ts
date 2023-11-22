9730ced84f0f7bb7e6984a088084e5edf81908c5
Bishop <christianiradukunda6@gmail.com>
2023-11-22 23:28:05 +0200

    worked with intersection

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
-// working with unions
-// a variable can be either a string or a number or any other defined type
-let id : string | number
-// we can re-assign the id as a number or a string
-id = "1"
-id=7
-
-// example2: using unions on default values
-type Todo ={
-    name:string,
-    status:"complete" | "in progress" | "draft"
+// working with Intersection
+// a type can have a combined data types
+type Person ={
+    name:string;
+    age:number;
 }
+type PersonWithId = Person & {id:string}
 
-// if we try to initialize a variable we have to only work with pre-defined statuses
-
-const todo:Todo = {name:"Fix bugs",status:"complete"}
+const person :PersonWithId = {id:"fjsdjflkdjkf",name:"Christian",age:30}
