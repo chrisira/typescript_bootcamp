356929bbc78c8f6e31a263e659545b92be968a26
Bishop <christianiradukunda6@gmail.com>
2023-11-22 23:31:56 +0200

    adding intersection in interfaces

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,15 @@ type Person ={
 type PersonWithId = Person & {id:string}
 
 const person :PersonWithId = {id:"fjsdjflkdjkf",name:"Christian",age:30}
+
+// using interface
+
+interface Person2{
+    name:string;
+    age:number;
+}
+interface PersonWithId2 extends Person2 {id:string}
+
+const person2 :PersonWithId = {id:"fjsdjflkdjkf",name:"Christian",age:30}
+
+
